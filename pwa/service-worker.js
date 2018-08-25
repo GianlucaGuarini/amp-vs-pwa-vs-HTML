b397e60b9c3445eb0bb4229680cbaa8d793aba12
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-08-25 11:23:07 +0200

    added: demos

---
+++ pwa/service-worker.js
@@ -0,0 +1,35 @@
+const dataCacheName = 'demoData-v1'
+const cacheName = 'demoPWA-final-1'
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/inline.css'
+]
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install')
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell')
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate')
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+  return self.clients.claim()
+})
+
+
